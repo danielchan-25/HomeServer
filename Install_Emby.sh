5f14379dd3b1f4343a25b28c059f34cf19ef60c7
司徒仲豪 <50611800+danielchan-25@users.noreply.github.com>
2022-09-28 17:17:12 +0800

    Add files via upload

---
+++ Install_Emby.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# https://emby.media/download.html
+
+wget https://github.com/MediaBrowser/Emby.Releases/releases/download/4.7.6.0/emby-server-deb_4.7.6.0_amd64.deb
+dpkg -i emby-server-deb_4.7.6.0_amd64.deb
